4487c80305d3bbab79c4b638f511f94459821f38
ignitz <ignitzhjfk@gmail.com>
2018-06-03 17:09:42 -0300

    JWT login OK

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,10 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 import User from "../models/user";
+import { RSA_PKCS1_OAEP_PADDING } from "constants";
 
 router.post("/signup", (req, res, next) => {
   User.find({ email: req.body.email })
@@ -42,6 +44,50 @@ router.post("/signup", (req, res, next) => {
           }
         });
       }
+    });
+});
+
+router.post("/login", (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Auth failed"
+          });
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id
+            },
+            process.env.JWT_KEY,
+            {
+              expiresIn: "1h"
+            }
+          );
+          return res.status(200).json({
+            message: "Auth successful",
+            token: token
+          });
+        }
+        return res.status(401).json({
+          message: "Auth failed"
+        });
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Error in POST /login",
+        error: err
+      });
     });
 });
 
